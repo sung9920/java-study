65e5b3be5c1ad721316280033cfbf46215936f76
sung9920 <sungcheol9920@gmail.com>
2025-07-23 04:58:58 +0900

    drill06 #8

--- drill06/src/main/java/drill06/prob08/ProbMain.java
+++ drill06/src/main/java/drill06/prob08/ProbMain.java
@@ -2,22 +2,29 @@ package drill06.prob08;
 
 import java.util.Scanner;
 
-import drill06.prob04.Car;
-
 public class ProbMain {
 
 	public static void main(String[] args) {
 		Score[] scores = new Score[5];
+		int sum = 0;
 
 		Scanner scanner = new Scanner(System.in);
 
 		for (int i = 0; i < 5; i++) {
 			System.out.println("점수 " + (i+1) + ": ");
-			scores[i] = scanner.nextInt();
-			cars[i] = new Car();
+			int score = scanner.nextInt();
+			scores[i] = new Score();
+			scores[i].setValue(score);
 		}
-		
 		scanner.close();
+
+
+		for (int i = 0; i < 5; i++) {
+			sum += scores[i].getValue();
+		}
+
+		double average = (double) sum / scores.length;
+		System.out.println("평균 점수: " + average);
 	}
 
 }
--- drill06/src/main/java/drill06/prob08/Score.java
+++ drill06/src/main/java/drill06/prob08/Score.java
@@ -2,4 +2,12 @@ package drill06.prob08;
 
 public class Score {
 	private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
