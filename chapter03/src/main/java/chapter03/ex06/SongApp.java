a2fea847cd8a51d84630fd88318c3311f93afc5f
sung9920 <sung9920@gmail.com>
2025-07-22 11:47:59 +0900

    method #1

--- chapter03/src/main/java/chapter03/ex06/SongApp.java
+++ chapter03/src/main/java/chapter03/ex06/SongApp.java
@@ -9,7 +9,7 @@ public class SongApp {
 		song1.setAlbum("Real, ");
 		song1.setArtist("아이유 ");
 		song1.setTitle("좋은날 ");
-		song1.setComposer(", 이민수 작곡");
+		song1.setComposer(", 이민수 작곡 ");
 		song1.setYear(2010);
 		song1.setTrack(3);
 		
