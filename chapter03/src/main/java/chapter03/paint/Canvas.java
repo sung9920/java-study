e828037b0ccd5aca0b2ebe60a943df575e1d78dd
sung9920 <sungcheol9920@gmail.com>
2025-07-22 12:25:19 +0900

    그림판 #feat

---
+++ chapter03/src/main/java/chapter03/paint/Canvas.java
@@ -0,0 +1,7 @@
+package chapter03.paint;
+
+public class Canvas {
+	public void drawPoint(Point point) {
+		point.show();
+	}
+}
---
+++ chapter03/src/main/java/chapter03/paint/Main.java
@@ -0,0 +1,20 @@
+package chapter03.paint;
+
+public class Main {
+	
+	    public static void main(String[] args) {
+	        Canvas canvas = new Canvas();
+
+	        // 사용자가 (10, 20) 위치를 클릭한 상황
+	        Point point1 = new Point();
+			point1.setX(10);
+			point1.setY(20);
+	        canvas.drawPoint(point1);
+
+	        // 사용자가 (100, 200) 위치를 클릭한 상황
+	        Point point2 = new Point();
+			point2.setX(100);
+			point2.setY(200);
+	        canvas.drawPoint(point2);
+	} 
+}
